d62defdcab79bbaffc2b609f998f048145f827b6
miraclee1226 <shl6534@naver.com>
2022-08-17 17:51:53 +0900

    디지털 시계 만들기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  let [time, setTime] = useState(new Date())
+
+
+  useEffect(()=>{
+    setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return (
+      clearInterval()
+      )
+  }, []);
+
+
+
   return (
-    <div className="App">
-      안녕~!!!!
+    <div className='App'>
+      <div className='nav'>
+        <h4>Pomodoro</h4>
+      </div>
+      <div className='date'>{time.toLocaleTimeString()}</div>
+      <div className='rightnav'>
+      </div>
+      
+
     </div>
+
   );
 }
+
+
 
 export default App;
