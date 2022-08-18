2af1bcb78a7bae34ec3ac5b1a460bf794d093fee
miraclee1226 <shl6534@naver.com>
2022-08-18 15:02:50 +0900

    Decorate navigation bar with css / Making to-do list using input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import {useState, useEffect} from 'react';
+import { Routes, Route, Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+
+
 
 function App() {
   let [time, setTime] = useState(new Date())
+  let [text, setText] = useState("")
+  let [list, setList] = useState([])
+  let [isValid, setIsValid] = useState(false)
 
-
+  
   useEffect(()=>{
     setInterval(() => {
       setTime(new Date());
@@ -12,8 +20,16 @@ function App() {
     return (
       clearInterval()
       )
-  }, []);
+    }, []);
 
+    function post (e) {
+      const copyList = [...list];
+      copyList.push(text);
+      setList(copyList);
+      setText('');
+
+    }
+    
 
 
   return (
@@ -22,13 +38,80 @@ function App() {
         <h4>Pomodoro</h4>
       </div>
       <div className='date'>{time.toLocaleTimeString()}</div>
-      <div className='rightnav'>
-      </div>
       
+    <Routes>
+
+      <Route path='/' element= {
+      <div className='todoContent'>
+        <Todo />
+        <div className='todayContent'>
+          <h1>오늘</h1>
+          <div className='todayMainContent'>
+            <p>완료한 시간</p>
+            <p>완료한 작업</p>
+          </div>
+          <div>
+            <input 
+              type="text" 
+              onChange={(e)=>{
+                setText(e.target.value);
+              }} 
+              onKeyUp={(e)=> {
+                e.target.value.length > 0
+                ? setIsValid(true) 
+                : setIsValid(false);
+              }}
+              value={text}
+              placeholder='✔ 할 일 추가' />
+              <button 
+              type='button'
+              onClick={post}
+              disabled={isValid ? false : true}
+              >버튼</button>
+          </div>
+        </div>
+      </div>
+         } />
+
+      <Route path='/tomorrowtodo' element= {
+        <div className='todoContent'>
+          <Todo />
+        </div>
+         } />
+
+      <Route path='/someday' element= {
+        <div className='todoContent'>
+          <Todo />
+        </div> } />
+
+      <Route path='/complete' element= {
+        <div className='todoContent'>
+          <Todo />
+        </div> } />
+
+    </Routes>
+    
 
     </div>
 
   );
+}
+
+function Todo(props) {
+  return(
+
+      <div className='leftnav'>
+        <div className='todoLink'>
+          <Link to ="/">🌞 오늘 할 일</Link>
+          <Link to="/tomorrowtodo">📆 내일 할 일</Link>
+          <Link to="/someday">📅 추후</Link>
+          <Link to="/complete">✅ 완료</Link>
+        </div>
+       </div>
+      
+
+
+  )
 }
 
 
