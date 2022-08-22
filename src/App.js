5965effcc1f2dc9e35c21652e2d99bd8a72480a3
miraclee1226 <shl6534@naver.com>
2022-08-22 13:08:50 +0900

    Make complete button

--- src/App.js
+++ src/App.js
@@ -8,10 +8,20 @@ import styled from 'styled-components'
 
 function App() {
   let [time, setTime] = useState(new Date())
-  let [text, setText] = useState("")
-  let [list, setList] = useState([])
-  let [isValid, setIsValid] = useState(false)
 
+  let [text, setText] = useState("")
+  let [text2, setText2] = useState("")
+
+  let [list, setList] = useState([])
+  let [list2, setList2] = useState([])
+
+  let [isValid, setIsValid] = useState(false)
+  let [isValid2, setIsValid2] = useState(false)
+
+  let [min, setMin] = useState(25)
+  let [sec, setSec] = useState(0)
+  let [done , setDone] = useState(0)
+  let [doneList, setDoneList] = useState([])
   
   useEffect(()=>{
     setInterval(() => {
@@ -28,7 +38,35 @@ function App() {
       setList(copyList);
       setText('');
     }
-    
+
+    function post2 (e) {
+      const copyList2 = [...list2];
+      copyList2.push(text2);
+      setList2(copyList2);
+      setText2('');
+    }
+
+    function startTimer() {
+      setInterval(() => {
+        setMin(min => min -1)
+      }, 1000);
+      
+
+      const countDown = setInterval(() => {
+        if(parseInt(sec > 0)) {
+          setSec(parseInt(sec)-1);
+        }
+        if(parseInt(sec === 0)){
+          if(parseInt(min === 0)) {
+            clearInterval(countDown);
+          } else {
+            setMin(parseInt(min) - 1);
+            setSec(59);
+          }
+        }
+      },1000);
+    }
+
 
 
   return (
@@ -44,10 +82,19 @@ function App() {
         <div className='todoContent'>
           <Todo />
           <div className='todayContent'>
-            <h1>오늘</h1>
-            <div className='todayMainContent'>
-              <p>완료한 시간<span>0</span></p>
-              <p>완료한 작업<span>0</span></p>
+            <div className='mainAndtimer'>
+              <div>
+                <h1>오늘</h1>
+                
+                <div className='todayMainContent'>
+                  <p>완료한 시간</p>
+                  <p>완료한 작업{done}</p>
+                </div>
+              </div>
+              <div className='timer'>
+                {min}:{sec < 10 ? `0${sec}` : sec}
+              <button onClick={startTimer}>Start</button>
+              </div>
             </div>
             <div className='textList'>
               <p>할 일</p>
@@ -77,7 +124,12 @@ function App() {
               list.map((textArr, i) => {
                 return (
                   <div className='todoList'>
-                    <button>완료</button>
+                    <button onClick={()=> {
+                      setDone(done+1)
+                      let copy = [...list];
+                      copy.splice(i, 1);
+                      setList(copy);
+                    }}>완료</button>
                     <p>{textArr}</p>
                     <button onClick={()=>{
                       let copy = [...list];
@@ -88,11 +140,14 @@ function App() {
                 )
               })
             }
-
-            
           </div>
         </div>
           } />
+
+
+
+
+
 
         <Route path='/tomorrowtodo' element= {
           <div className='todoContent'>
@@ -106,33 +161,32 @@ function App() {
                   <input 
                     type="text" 
                     onChange={(e)=>{
-                      setText(e.target.value);
+                      setText2(e.target.value);
                     }} 
                     onKeyUp={(e)=> {
                       e.target.value.length > 0
-                      ? setIsValid(true) 
-                      : setIsValid(false);
+                      ? setIsValid2(true) 
+                      : setIsValid2(false);
                     }}
-                    value={text}
+                    value={text2}
                     placeholder='✔ 할 일 추가' />
                     <button 
                     type='button'
-                    onClick={post}
-                    disabled={isValid ? false : true}
+                    onClick={post2}
+                    disabled={isValid2 ? false : true}
                     >버튼</button>
                 </div>
               </div>
 
               {
-              list.map((textArr, i) => {
+              list2.map((textArr2, i) => {
                 return (
                   <div className='todoList'>
-                    <button>완료</button>
-                    <p>{textArr}</p>
+                    <p>{textArr2}</p>
                     <button onClick={()=>{
-                      let copy = [...list];
-                      copy.splice(i, 1);
-                      setList(copy);
+                      let copy2 = [...list2];
+                      copy2.splice(i, 1);
+                      setList2(copy2);
                     }}>삭제</button>
                   </div>
                 )
