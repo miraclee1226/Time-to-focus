2b6cc30596c25bf0df26142b795b2a0dd7964e40
miraclee1226 <shl6534@naver.com>
2022-08-23 16:26:06 +0900

    Add input

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,18 @@
-import {useState, useEffect} from 'react';
+import userEvent from '@testing-library/user-event';
+import {useState, useEffect, Link} from 'react';
+
+
 
 function Home () {
     let [time, setTime] = useState(new Date())
     let [text, setText] = useState("")
-    let [text2, setText2] = useState("")
-
     let [list, setList] = useState([])
-    let [list2, setList2] = useState([])
-
     let [isValid, setIsValid] = useState(false)
-    let [isValid2, setIsValid2] = useState(false)
-
-    let [min, setMin] = useState(25)
-    let [sec, setSec] = useState(0)
     let [done , setDone] = useState(0)
-    let [doneList, setDoneList] = useState([])
+    let [min, setMin] = useState('')
+    let [sec, setSec] = useState('0')
+    
+    
 
     useEffect(()=>{
     setInterval(() => {
@@ -32,33 +30,14 @@ function Home () {
         setText('');
     }
 
-    function post2 (e) {
-        const copyList2 = [...list2];
-        copyList2.push(text2);
-        setList2(copyList2);
-        setText2('');
+    const onMinChange = (e)=> {
+        setMin(e.target.value);
     }
 
-    function startTimer() {
-        setInterval(() => {
-        setMin(min => min -1)
-        }, 1000);
-        
-
-        const countDown = setInterval(() => {
-        if(parseInt(sec > 0)) {
-            setSec(parseInt(sec)-1);
-        }
-        if(parseInt(sec === 0)){
-            if(parseInt(min === 0)) {
-            clearInterval(countDown);
-            } else {
-            setMin(parseInt(min) - 1);
-            setSec(59);
-            }
-        }
-        },1000);
+    const onSecChange = (e)=> {
+        setSec(e.target.value);
     }
+
     return (
         <div className='todoContent'>
             
@@ -73,10 +52,26 @@ function Home () {
                 </div>
                 </div>
                 <div className='timer'>
-                {min}:{sec < 10 ? `0${sec}` : sec}
-                <button onClick={startTimer}>Start</button>
+                    <input 
+                        type='number'
+                        placeholder= '0'
+                        value={min}
+                        onChange= {onMinChange}
+                        />
+                    <span>m</span>
+                    <input 
+                        type='number'
+                        placeholder= '0'
+                        value={sec}
+                        onChange= {onSecChange}
+                        />
+                    <span>s</span>
+                    <button>start</button>
+                    
                 </div>
             </div>
+
+            {/* 할 일 */}
             <div className='textList'>
                 <p>할 일</p>
                 <div className='inputButton'>
