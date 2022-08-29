7ac7f5832f29f0ac345f7557142a280470e0d1a8
miraclee1226 <shl6534@naver.com>
2022-08-29 11:13:11 +0900

    Add addDone, removeDone props

--- src/TodoComponents/TodoList.js
+++ src/TodoComponents/TodoList.js
@@ -1,11 +1,11 @@
 import TodoListItem from "./TodoListItem"
 
-function TodoList({todos, onRemove, onToggle}) {
+function TodoList({todos, onRemove, onToggle, addDone, removeDone, }) {
   return(
     <div>
       {
       todos.map(todo => (
-        <TodoListItem {...todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} /> 
+        <TodoListItem {...todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} addDone={addDone} removeDone={removeDone} /> 
     ))}
     </div>
    
