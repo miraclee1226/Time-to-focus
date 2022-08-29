4518c074d31f4d9cd0d314f346741a36acaa70ec
miraclee1226 <shl6534@naver.com>
2022-08-29 11:11:11 +0900

    Insert delete icon

--- src/pages/Tomorrow.js
+++ src/pages/Tomorrow.js
@@ -1,22 +1,14 @@
 import {useState, useEffect} from 'react';
+import { FaRegTrashAlt } from "react-icons/fa"
 
 function Tomorrow() {
-    let [time, setTime] = useState(new Date())
+
     let [text2, setText2] = useState("")
-
     let [list2, setList2] = useState([])
-
     let [isValid2, setIsValid2] = useState(false)
 
 
-    useEffect(()=>{
-    setInterval(() => {
-        setTime(new Date());
-    }, 1000);
-    return (
-        clearInterval()
-        )
-    }, []);
+
 
 
     function post2 (e) {
@@ -25,54 +17,61 @@ function Tomorrow() {
         setList2(copyList2);
         setText2('');
     }
+
         
 
     return (
-        <div className='todoContent'>
-      
-      <div className='todayContent'>
-        <h1>내일</h1>
+      <div className='todoContent'>
+        <div className='todayContent'>
+          <h1>내일</h1>
 
-        <div className='textList'>
-          <p>할 일</p>
-          <div className='inputButton'>
-            <input 
-              type="text" 
-              onChange={(e)=>{
-                setText2(e.target.value);
-              }} 
-              onKeyUp={(e)=> {
-                e.target.value.length > 0
-                ? setIsValid2(true) 
-                : setIsValid2(false);
-              }}
-              value={text2}
-              placeholder='✔ 할 일 추가' />
-              <button 
-              type='button'
-              onClick={post2}
-              disabled={isValid2 ? false : true}
-              >버튼</button>
-          </div>
-        </div>
-
-        {
-        list2.map((textArr2, i) => {
-          return (
-            <div className='todoList'>
-              <p>{textArr2}</p>
-              <button onClick={()=>{
-                let copy2 = [...list2];
-                copy2.splice(i, 1);
-                setList2(copy2);
-              }}>삭제</button>
+          <div className='textList'>
+            <p>할 일</p>
+            <div className='inputButton'>
+              <input 
+                type="text" 
+                onChange={(e)=>{
+                  setText2(e.target.value);
+                }} 
+                onKeyUp={(e)=> {
+                  e.target.value.length > 0
+                  ? setIsValid2(true) 
+                  : setIsValid2(false);
+                  if(window.event.keyCode == 13){
+                    post2()
+                }
+                }}
+                value={text2}
+                placeholder='✔ 할 일 추가' />
+                <button 
+                type='button'
+                onClick={post2}
+                disabled={isValid2 ? false : true}
+                >버튼</button>
             </div>
-          )
-        })
-      }
+          </div>
 
+          {
+          list2.map((textArr2, i) => {
+            return (
+              <div className='todolistitemStyle'>
+                <div className='checkboxAndcontent'>
+                  <p>{textArr2}</p>
+                  </div>
+                  <div className='removeboxDiv'>
+                    <FaRegTrashAlt className='removebox' onClick={()=>{
+                      let copy2 = [...list2];
+                      copy2.splice(i, 1);
+                      setList2(copy2);
+                    }}/>
+                  </div>
+              </div>
+            )
+          })
+        }
+
+        </div>
       </div>
-    </div>
     )
     }
 
