8f013c7a5047b3114cd1a15ce4f6b079884848c4
miraclee1226 <shl6534@naver.com>
2022-09-02 17:32:54 +0900

    .

--- src/TodoComponents/TodoList.js
+++ src/TodoComponents/TodoList.js
@@ -1,12 +1,14 @@
 import TodoListItem from "./TodoListItem"
 
-function TodoList({todos, onRemove, onToggle, addDone, removeDone, }) {
+function TodoList({todos, onRemove, onToggle, addDone, removeDone, checkedList}) {
   return(
     <div>
       {
-      todos.map(todo => (
-        <TodoListItem {...todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} addDone={addDone} removeDone={removeDone} /> 
-    ))}
+        todos.map(todo => (
+          <TodoListItem {...todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} addDone={addDone} removeDone={removeDone} addDone={addDone} />         
+        )
+        )
+      }
     </div>
    
   )
