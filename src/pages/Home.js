d116dc705537aca5c055ad1f69a623f919d72bb1
miraclee1226 <shl6534@naver.com>
2022-09-02 17:35:06 +0900

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,24 @@
 // import userEvent from '@testing-library/user-event';
 import React from 'react';
-import {useState, useEffect, useRef, Link} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import Timer from './Timer'
 import TodoInsert from '../TodoComponents/TodoInsert';
 import TodoList from '../TodoComponents/TodoList';
-import TodoListItem from '../TodoComponents/TodoListItem';
-import Complete from './Complete';
+
 // import { noop } from '@tanstack/query-core/build/types/packages/query-core/src/utils';
 
 
 function Home () {
-    let [time, setTime] = useState(new Date())
     let min = useState(25)
     let sec = useState(0)
-    let restTime = useState(3)
-    let min2 = useState(10)
-    let sec2 = useState(0)
     let [done , setDone] = useState(0)
     let [todos, setTodos] = useState([])
-    let [complete, setComplete] = useState([])
 
     function addTodo(text) {
         setTodos([
+            // 이전에 있던 목록은 그대로 유지하면서
             ...todos,
+            // 새로운 목록을 추가한 배열을 생성함
             {
                 id: Math.random().toString(), textValue: text, checked: false
             },
@@ -31,7 +27,7 @@ function Home () {
 
     const onRemove = function (id) {
         return function (e) {
-            return setTodos(todos.filter(todo => todo.id !== id))
+             setTodos(todos.filter(todo => todo.id !== id))
         } 
     }
     
@@ -42,6 +38,7 @@ function Home () {
                     todo.id === id ? {...todo, checked: !todo.checked} : todo
                 )
             )
+            // console.log(todos)
         }
     }
 
@@ -49,26 +46,10 @@ function Home () {
         setDone(done+1)
     }
 
-    function removeDone(text) {
+    function removeDone() {
         setDone(done-1)
-        setComplete([
-            ...complete,
-            {
-                id: Math.random().toString(), textValue: text, checked: true
-            }
-        ])
-
     }
-
-    useEffect(()=>{
-    setInterval(() => {
-        setTime(new Date());
-    }, 1000);
-    return (
-        clearInterval()
-        )
-    }, []);
-
+    
     return (
         <div className='todoContent'>
             <div className='todayContent'>
@@ -81,7 +62,7 @@ function Home () {
                         </div>
                     </div>
                     <div className='homeTimer'>
-                        <Timer min={min} sec={sec} min2={min2} sec2={sec2} restTime={restTime} />
+                        <Timer min={min} sec={sec} />
                     </div>
                 </div>
             </div>
@@ -93,6 +74,8 @@ function Home () {
                     <div className='inputButton'>
                         <TodoInsert onAddTodo={addTodo} />
                         <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} addDone={addDone} removeDone={removeDone} />
+
+
                     </div>
                 </div>
             </div>
