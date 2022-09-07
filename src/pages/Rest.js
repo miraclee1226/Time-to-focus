9fc98de6f6b5274c7e3535d94253cb0a50f3a30a
miraclee1226 <shl6534@naver.com>
2022-09-07 14:25:40 +0900

    Add div

--- src/pages/Rest.js
+++ src/pages/Rest.js
@@ -3,15 +3,20 @@ import ReactPlayer from 'react-player';
 
 function Rest() {
   return (
+    <div className='todoContent'>
+      <div className='todayContent'>
+        <h1 className='restTitle'>휴식</h1>
+      </div>
     <div className='player-wrapper'>
     <ReactPlayer 
         className="react-player"
         url={"https://www.youtube.com/watch?v=9LQsLPsKRq8"} 
         width="1100px"
-        height="570px"
+        height="500px"
         playing={true}
         muted={true}
         controls={true} />
+    </div>
     </div>
   )
 }
