01507b350e4042f3d1927f96460e5c4ca8fffc00
miraclee1226 <shl6534@naver.com>
2022-09-13 08:09:10 +0900

    Create an input function that can be modified (1)

--- src/TodoComponents/TodoListItem.js
+++ src/TodoComponents/TodoListItem.js
@@ -1,21 +1,76 @@
+import { useEffect, useState, useRef } from 'react';
 import { MdOutlineCheckBox } from "react-icons/md"
 import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md"
 import { FaRegTrashAlt } from "react-icons/fa"
 import React from "react";
 
-function TodoListItem({textValue, id, checked, onRemove, onToggle, addDone, removeDone}) {
+function TodoListItem({todos, setTodos, textValue, id, checked, onRemove, onToggle, addDone, removeDone}) {
+
+  let [edited, setedited] = useState(false);
+  const editInputRef = useRef(null);
+  let [newText, setnewText] = useState(todos.textValue);
+  let [isValid3, setIsValid3] = useState(false);
+
+  useEffect(()=> {
+    //edit 모드일 때 포커싱 한다
+    if(edited) {
+      editInputRef.current.focus();
+    }
+  }, [edited]);
+
+  function onClickEditBtn() {
+    setedited(true);
+  }
+
+  function onChangeEditInput(e) {
+    setnewText(e.target.value);
+  }
+
+  function onDoubleClick() {
+    const nextTodoList = todos.map(todo => ({ 
+        ...todo,
+        textValue: todo.id === id ? newText : todo.textValue,
+    }));
+    setTodos(nextTodoList);
+
+    setedited(false);
+  }
 
   return (
     <div className='todolistitemStyle'>
       <div className='checkboxAndcontent'>
           <div onClick={onToggle(id)} className='checkbox'>  
           { 
-          checked ? 
+          checked ? // checked: false;
           <MdOutlineCheckBox onClick={removeDone} className='checkbox'/> : 
-          <MdOutlineCheckBoxOutlineBlank onClick={addDone} className='checkbox'/>
+          <MdOutlineCheckBoxOutlineBlank onClick={addDone} className='checkbox'/> 
           }
           </div>
-          <p className={checked ? 'line' : 'noLine'}>{textValue}</p>
+          {
+            !checked ? ( // 체크  안 된 상태일 때
+              edited ? (  // 편집 안 된 상태일 때
+                <input 
+                type="text" 
+                value={newText}
+                ref={editInputRef}
+                onDoubleClick={onDoubleClick} 
+                onChange={onChangeEditInput}
+                onKeyUp={(e)=> { 
+                  if(window.event.keyCode == 13) {
+                    if(e.target.value.length > 0) {
+                        setIsValid3(true) 
+                        onDoubleClick()
+                      }
+                  } else {
+                    setIsValid3(false)
+                  }
+                }}
+                /> 
+                ) : ( // 수정 된 상태일 때
+                  <p onClick={onClickEditBtn} className={checked ? 'line' : 'noLine'}>{textValue}</p>
+              )
+              ) : <p className={checked ? 'line' : 'noLine'}>{textValue}</p>  
+          }
       </div>
       <div className='removeboxDiv'>
         <FaRegTrashAlt className='removebox' onClick={onRemove(id)}/>
