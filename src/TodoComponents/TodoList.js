39da6b6668ac7c0c1fd0d7fee404f9a81f632d0d
miraclee1226 <shl6534@naver.com>
2022-09-13 08:10:11 +0900

    Add todo, setTodo props to TodoListItem Component

--- src/TodoComponents/TodoList.js
+++ src/TodoComponents/TodoList.js
@@ -1,12 +1,12 @@
 import React from "react"
 import TodoListItem from "./TodoListItem"
 
-function TodoList({todos, onRemove, onToggle, addDone, removeDone}) {
+function TodoList({todos, setTodos, onRemove, onToggle, addDone, removeDone}) {
   return(
     <div>
       {
         todos.map(todo => (
-          <TodoListItem {...todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} addDone={addDone} removeDone={removeDone} />         
+          <TodoListItem {...todo} key={todo.id} todos={todos} setTodos={setTodos} onRemove={onRemove} onToggle={onToggle} addDone={addDone} removeDone={removeDone} />         
         )
         )
       }
