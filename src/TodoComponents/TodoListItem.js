5f3e1bf24ef001d0ec7b8867f344c05ba6efc16a
miraclee1226 <shl6534@naver.com>
2022-09-15 13:43:32 +0900

    Prevent submission if nothing is entered

--- src/TodoComponents/TodoListItem.js
+++ src/TodoComponents/TodoListItem.js
@@ -18,6 +18,10 @@ function TodoListItem({todos, setTodos, textValue, id, checked, onRemove, onTogg
     }
   }, [edited]);
 
+  function handleInputBlur() {
+    <p onDoubleClick={onClickEditBtn} className={checked ? 'line' : 'noLine'}>{textValue}</p>
+  }
+
   function onClickEditBtn() {
     setedited(true);
   }
@@ -36,6 +40,21 @@ function TodoListItem({todos, setTodos, textValue, id, checked, onRemove, onTogg
     setedited(false);
   }
 
+  function onKeyUp (e) {
+    e.preventDefault();
+    let blankPattern = /^\s+|\s+$/g;
+    if(window.event.keyCode == 13) {
+      if(e.target.value.length > 0) {
+        setIsValid3(true) 
+        onDoubleClick()
+      } else if(e.target.value.replace(blankPattern,'') == ""){
+        return false;
+      }
+    } else {
+      setIsValid3(false)
+    }
+  }
+
   return (
     <div className='todolistitemStyle'>
       <div className='checkboxAndcontent'>
@@ -51,23 +70,15 @@ function TodoListItem({todos, setTodos, textValue, id, checked, onRemove, onTogg
               edited ? (  // 편집 안 된 상태일 때
                 <input 
                 type="text" 
-                value={newText}
+                value={newText || '' || undefined}
+                className='editedInput'
                 ref={editInputRef}
-                onDoubleClick={onDoubleClick} 
                 onChange={onChangeEditInput}
-                onKeyUp={(e)=> { 
-                  if(window.event.keyCode == 13) {
-                    if(e.target.value.length > 0) {
-                        setIsValid3(true) 
-                        onDoubleClick()
-                      }
-                  } else {
-                    setIsValid3(false)
-                  }
-                }}
+                onKeyUp={onKeyUp}
+                onBlur={handleInputBlur}
                 /> 
                 ) : ( // 수정 된 상태일 때
-                  <p onClick={onClickEditBtn} className={checked ? 'line' : 'noLine'}>{textValue}</p>
+                  <p onDoubleClick={onClickEditBtn} className={checked ? 'line' : 'noLine'}>{textValue}</p>
               )
               ) : <p className={checked ? 'line' : 'noLine'}>{textValue}</p>  
           }
