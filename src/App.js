f2fa0b599b46c4d53e350e670d6ea595e777454f
miraclee1226 <shl6534@naver.com>
2022-09-21 14:03:57 +0900

    Rename classNames

--- src/App.js
+++ src/App.js
@@ -2,51 +2,50 @@ import './App.css';
 import React from 'react';
 import {useState, useEffect} from 'react';
 import {Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/Home'
+import Today from './pages/Today'
 import Tomorrow  from './pages/Tomorrow';
 import Someday from './pages/Someday'
 import Rest from './pages/Rest';
 
 
+
 function App() {
   let [time, setTime] = useState(new Date())
 
-    useEffect(()=>{
-    setInterval(() => {
+  useEffect(()=>{
+    const timer = setInterval(() => {
         setTime(new Date());
     }, 1000);
-    return (
-        clearInterval()
-        )
-    }, []);
+    return ()=>{
+      clearInterval(timer)
+    }
+    }, []); 
 
   return (
-    <div className='scrollBar'>
-      <div className='nav'>
+    <div className='app'>
+      <div className='header'>
         <h4>Time To Focus</h4>
       </div>
 
       <div className='allContent'>
-          <div className='leftContainer'>
-            <div className='leftnav'>
-              <div className='todoLink'>
-                <Link to ="/">🌞 오늘 할 일</Link>
-                <Link to="/tomorrowtodo">📆 내일 할 일</Link>
-                <Link to="/someday">📅 추후</Link>
-                <Link to="/rest">🙌 휴식</Link>
-              </div>
-            </div>
-          <div className='date'>{time.toLocaleTimeString()}</div>
+        <div className='leftNav'>
+          <div className='leftNavLink'>
+            <Link to ="/">🌞 오늘 할 일</Link>
+            <Link to="/tomorrowtodo">📆 내일 할 일</Link>
+            <Link to="/someday">📅 추후</Link>
+            <Link to="/rest">🙌 휴식</Link>
           </div>
-        
-
+          <div className='nowTime'>
+            {time.toLocaleTimeString()}
+          </div>
+        </div>
+      </div>
         <Routes>
-          <Route path='/' element= {<Home />} />
+          <Route path='/' element= {<Today />} />
           <Route path='/tomorrowtodo' element= {<Tomorrow />} />
           <Route path='/someday' element= {<Someday />} />
           <Route path='/rest' element= {<Rest />} />
         </Routes>
-      </div>
     </div>
   );
 }
