a3bf2c60b902302ac34f3dc2f079c999ac3d2776
miraclee1226 <shl6534@naver.com>
2022-09-23 23:22:17 +0900

    Delete useState(isValid)

--- src/TodoComponents/TodoInsert.js
+++ src/TodoComponents/TodoInsert.js
@@ -3,8 +3,6 @@ import React, {useState} from 'react';
 
 function TodoInsert({onAddTodo}) {
   let [newTodoItem, setNewTodoItem] = useState('');
-  let [isValid3, setIsValid3] = useState(false);
-  // let [toggle, setToggle] = useState(true);
 
   function todoInputHandler(e) {
     setNewTodoItem(e.target.value);
@@ -20,16 +18,14 @@ function TodoInsert({onAddTodo}) {
     <input 
       className='insertBox'
       onKeyUp={(e)=> { 
-            e.preventDefault();
-        if(window.event.keyCode == 13) {
-          if(e.target.value.length > 0) {
-              setIsValid3(true) 
-              addTodoHandler()
-            }
-        } else {
-          setIsValid3(false)
-        }
-      }} onChange={todoInputHandler} value={newTodoItem} placeholder='✔ 할 일 추가 (Press Enter)' />
+        e.preventDefault();
+        if(e.key == 'Enter' && e.target.value.length > 0) {
+          addTodoHandler()
+        } 
+      }} 
+      onChange={todoInputHandler} 
+      value={newTodoItem} 
+      placeholder='✔ 할 일 추가 (Press Enter)' />
   </div>
   )
 }
