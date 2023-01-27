88ca8d4165f32c4dda5efd074849f814f39d5266
miraclee1226 <shl6534@naver.com>
2023-01-27 20:13:42 +0900

    add iframe

--- src/pages/Rest.js
+++ src/pages/Rest.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactPlayer from 'react-player';
 
 function Rest() {
   return (
@@ -7,16 +6,17 @@ function Rest() {
       <div className='todayContent'>
         <h1 className='restTitle'>휴식</h1>
       </div>
-    <div className='player-wrapper'>
-    <ReactPlayer 
-        className="react-player"
-        url={"https://www.youtube.com/watch?v=jANE8lpoj2c"} 
-        width="1100px"
-        height="500px"
-        playing={true}
-        muted={true}
-        controls={true} />
-    </div>
+      <div className='player-wrapper'>
+        <iframe className='iframe'
+          width="1100" 
+          height="500" 
+          src="https://www.youtube.com/embed/jANE8lpoj2c?autoplay=1&mute=1" 
+          title="YouTube video player" 
+          // frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          allowfullscreen>
+        </iframe>
+      </div>
     </div>
   )
 }
